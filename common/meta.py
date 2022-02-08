78bed3457a4590bf1367d1a4ae56f9b669fbbc13
Екатерина Осенина <ekaterinaosenina@macbook.osenin.me>
2022-02-08 10:17:15 +0300

    clientmaker

--- common/meta.py
+++ common/meta.py
@@ -23,3 +23,26 @@ class ServerMaker(type):
             raise TypeError('Некорректная инициализация сокета.')
 
         super().__init__(clsname, bases, clsdict)
+
+
+# Metaclass for client
+class ClientMaker(type):
+    def __init__(self, clsname, bases, clsdict):
+        methods = []
+        for item in clsdict:
+            try:
+                instructions = dis.get_instructions(clsdict[item])
+            except TypeError:
+                pass
+            else:
+                for i in instructions:
+                    if i.opname == 'LOAD_GLOBAL' and i.argval not in methods:
+                        methods.append(i.argval)
+        for command in ('accept', 'listen', 'socket'):
+            if command in methods:
+                raise TypeError('В классе обнаружено использование запрещённого метода')
+        if 'get_message' in methods or 'send_message' in methods:
+            pass
+        else:
+            raise TypeError('Отсутствуют вызовы функций, работающих с сокетами.')
+        super().__init__(clsname, bases, clsdict)
